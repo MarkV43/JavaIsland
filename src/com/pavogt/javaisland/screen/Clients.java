8d6ab3da11493e9730b7cda4643101b7121f2edc
MarkV43 <marcelinho.vogt@gmail.com>
2021-04-06 18:51:22 -0300

    Trabalho nos Clientes

--- src/com/pavogt/javaisland/screen/Clients.java
+++ src/com/pavogt/javaisland/screen/Clients.java
@@ -5,14 +5,26 @@ import java.awt.*;
 public class Clients extends Panel {
     public Clients() {
         Button bAdd = new Button("+");
-        bAdd.setBounds(50, 50, 100, 100);
-        bAdd.setBackground(Color.BLACK);
-        bAdd.setForeground(Color.WHITE);
-        Font font = new Font("Rockwell Nova", Font.PLAIN, 50);
+        bAdd.setBounds(20, 20, 30, 30);
+        Font font = new Font("Rockwell Nova", Font.PLAIN, 28);
         bAdd.setFont(font);
         add(bAdd);
 
-        setBackground(Color.BLUE);
+        TextArea search = new TextArea("", 1, 30, TextArea.SCROLLBARS_NONE);
+        search.setBounds(60, 20, 300, 30);
+        search.setFont(new Font("Rockwell Nova", Font.PLAIN, 18));
+        add(search);
+
+        List list = new List(3, false);
+        list.add("Marcelo");
+        list.add("Gaby");
+        list.add("Luís Eduardo");
+        list.add("Eduardo");
+        list.add("Gabriel");
+        list.add("Marechal Luciolo");
+        list.setBounds(20, 60, 340, 580);
+        add(list);
+
         setLayout(null);
         setBounds(0, 90, 1280, 660);
     }
