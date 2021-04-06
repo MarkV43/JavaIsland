405272c34b297c1000cf17e99d63b196c323ae07
Luís Eduardo <luiseduparise@gmail.com>
2021-04-06 16:04:00 -0300

    Tela Admin melhorada

--- src/com/pavogt/javaisland/screen/Admin.java
+++ src/com/pavogt/javaisland/screen/Admin.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Admin extends Panel {
     public Admin(){
         Button bAdd = new Button("+");
-        bAdd.setBounds(50, 50, 100, 100);
+        bAdd.setBounds(50, 50, 30, 30);
         bAdd.setBackground(Color.GREEN);
         bAdd.setForeground(Color.BLACK);
         Font font = new Font("Rockwell Nova", Font.PLAIN, 50);
@@ -14,17 +14,37 @@ public class Admin extends Panel {
         add(bAdd);
 
         Button bRem = new Button("-");
-        bRem.setBounds(400,50,100,100);
+        bRem.setBounds(400,50,30,30);
         bRem.setBackground(Color.RED);
         bRem.setForeground(Color.BLACK);
         bRem.setFont(font);
         add(bRem);
 
         TextArea search = new TextArea("",1,100, TextArea.SCROLLBARS_NONE);
-        search.setBounds(170,50, 218,100);
+        search.setBounds(100,50, 288,30);
         search.setFont(font2);
         add(search);
 
+        List results = new List(100,false);
+        results.setBounds(100,100, 288,300);
+        add(results);
+
+        List stock = new List(100, false);
+        stock.add("RTX 3080Ti");
+        stock.add("RTX 3070Ti");
+        stock.add("RTX 3060Ti");
+        stock.add("RTX 3060");
+        stock.add("Mouse Redragon");
+        stock.add("Cyanide");
+        stock.add("RGB Strip Corsair");
+        stock.add("Revólver");
+        stock.add("Cute Dog");
+
+        stock.setBounds(600,50,500,400);
+        add(stock);
+
+
+        setBackground(Color.cyan);
         setLayout(null);
         setBounds(0, 90, 1280, 660);
     }
