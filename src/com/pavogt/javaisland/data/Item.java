e0f6d4da9f8dae112bb883774c492a1cf6da8e24
Luís Eduardo <luiseduparise@gmail.com>
2021-04-06 16:09:53 -0300

    Classe Item criada

---
+++ src/com/pavogt/javaisland/data/Item.java
@@ -0,0 +1,60 @@
+package com.pavogt.javaisland.data;
+
+import java.io.Serializable;
+
+public class Item implements Serializable {
+
+    private String name;
+    private float price;
+    private int quantity;
+    private String description;
+
+    public Item(String name, float price, int quantity, String description) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
