7a898a9dd81130f59249831375490bff4e5a2984
MarkV43 <marcelinho.vogt@gmail.com>
2021-04-06 19:01:23 -0300

    Adicionada classe Client

---
+++ src/com/pavogt/javaisland/data/Client.java
@@ -0,0 +1,64 @@
+package com.pavogt.javaisland.data;
+
+import java.io.Serializable;
+
+public class Client implements Serializable {
+
+    private String name;
+    private String email;
+    private long password;
+    private float balance;
+
+    public Client(String name, String email, long password, float balance) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public Client(String name, String email, long password) {
+        this(name, email, password, 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPassword() {
+        return password;
+    }
+
+    public void setPassword(long password) {
+        this.password = password;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password=" + password +
+                ", balance=" + balance +
+                '}';
+    }
+}
