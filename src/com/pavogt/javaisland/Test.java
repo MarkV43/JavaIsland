d5a39cfeef8bfbad552ce57ddfeb14a9cb689fa3
MarkV43 <marcelinho.vogt@gmail.com>
2021-04-07 15:26:04 -0300

    Added Test Class for testing serialization

---
+++ src/com/pavogt/javaisland/Test.java
@@ -0,0 +1,29 @@
+package com.pavogt.javaisland;
+
+import com.pavogt.javaisland.data.Client;
+
+import java.io.*;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Client[] clients = new Client[2];
+        clients[0] = new Client("Marcelo", "marcelo.vogt@grad.ufsc.br", 0, 99999);
+        clients[1] = new Client("Luís Eduardo", "luis.e.parise@grad.ufsc.br", 0, 69);
+
+        var fileOutputStream = new FileOutputStream("yourfile.txt");
+        var objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(clients);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+
+        var fileInputStream = new FileInputStream("yourfile.txt");
+        var objectInputStream = new ObjectInputStream(fileInputStream);
+        Client[] cs = (Client[]) objectInputStream.readObject();
+        objectInputStream.close();
+
+        for (Client c: cs) {
+            System.out.println(c);
+        }
+    }
+}
